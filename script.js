9caf7a6d65c63885a273d519c76211876fb46227
hadq <doquangha111@gmail.com>
2022-09-05 16:14:26 +0700

    add region config and flag check public api

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ const noti_bot = require('noti_bot')
 const notifyTelegram = noti_bot.telegram
 const notifySlack = noti_bot.slack
 
-const { 
-    getHealthCheckData, 
+const {
+    getHealthCheckData,
     getPublicApiHealthCheckData,
     OK,
     ERROR
- } = require('./index.js')
+} = require('./index.js')
 
 
 const main = async () => {
@@ -31,27 +31,36 @@ const main = async () => {
     }
 
     // PUBLIC API
-    let publicApiData = await getPublicApiHealthCheckData(process.env.PUBLIC_API_ENDPOINT)
-    if (publicApiData && publicApiData.length) {
-        for (const e of publicApiData) {
-            if (e.status === ERROR) {
-                errors.push(...e.errors)
+    if (process.env.IS_CHECK_PUBLIC_API === 'true') {
+        let publicApiData = await getPublicApiHealthCheckData(process.env.PUBLIC_API_ENDPOINT)
+        if (publicApiData && publicApiData.length) {
+            for (const e of publicApiData) {
+                if (e.status === ERROR) {
+                    errors.push(...e.errors)
+                }
             }
         }
+    }
+
+    let region = process.env.REGION;
+    if (region) {
+        errors.forEach((element, index) => {
+            errors[index] = `[${region}] ${element}`;
+        });
     }
 
     // sending notifications
     if (errors.length > 0) {
         let msg = process.env.PREFIX_MESSAGE + "\n" + errors.join("\n")
-        notifySlack(msg, process.env.SLACK_HOOK_KEY, process.env.SLACK_CHANNEL, process.env.SLACK_BOTNAME, process.env.SLACK_BOT_ICON)
+        // notifySlack(msg, process.env.SLACK_HOOK_KEY, process.env.SLACK_CHANNEL, process.env.SLACK_BOTNAME, process.env.SLACK_BOT_ICON)
         notifyTelegram(msg, process.env.TELEGRAM_TOKEN, process.env.TELEGRAM_CHAT, true)
     }
 
-    // console.log
-    if (errors.length > 0) {
-        let msg = errors.join("\n");
-        console.log(msg);
-    }
+    // console.log --- just for local testing
+    // if (errors.length > 0) {
+    //     let msg = errors.join("\n");
+    //     console.log(msg);
+    // }
 
     let end = Date.now();
     console.log(`Finish, check process took ${end - start} ms`);
