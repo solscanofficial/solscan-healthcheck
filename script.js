8e2ffa180df2c2fbee4ae974e6c46f2f63cd8b47
hadq <hado@solscan.io>
2024-06-03 09:17:17 +0700

    disable slack notification

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ require('dotenv').config({
 })
 const noti_bot = require('noti_bot')
 const notifyTelegram = noti_bot.telegram
-const notifySlack = noti_bot.slack
 const {createClient} = require('@clickhouse/client');
 const {
     checkAPI,
@@ -166,13 +165,9 @@ const main = async () => {
 
     // sending notifications
     if (errors.length > 0) {
-        let msgSlack = process.env.PREFIX_MESSAGE + "\n" + errors.join("\n")
-        // let msgTele = process.env.PREFIX_MESSAGE + "\n" + errors.join("\n\n")
-        notifySlack(msgSlack, process.env.SLACK_HOOK_KEY, process.env.SLACK_CHANNEL, process.env.SLACK_BOTNAME, process.env.SLACK_BOT_ICON)
         for (let error of errors) {
             notifyTelegram(error, process.env.TELEGRAM_TOKEN, process.env.TELEGRAM_CHAT, true)
         }
-
     }
 
     // console.log --- just for local testing
